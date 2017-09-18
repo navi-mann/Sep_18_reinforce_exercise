80c3f4f19c59949b0aca52619e848e2663ceb530
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-18 11:47:03 -0400

    added total votes

---
+++ exercise.rb
@@ -0,0 +1,18 @@
+ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
+           {1 => 'Bella', 2 => 'Lucky', 3 => 'Tigger'},
+           {1 => 'Bella', 2 => 'Boots', 3 => 'Smudge'},
+           {1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
+           {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
+
+
+
+total_votes = Hash.new(0)
+ballots.each_with_index do |ballot|
+ new_ballot = ballot.invert
+  new_ballot.each  do |name,points|
+  total_votes[name] +=points
+  end
+end
+
+puts total_votes
